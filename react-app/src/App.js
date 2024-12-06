f30ee080f2578fdb014e680878523f0ba7b7a828
sireesha0904 <modisireesha09@gmail.com>
2024-12-06 19:36:26 +0530

    updated

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,13 +1,23 @@
-
 import MyButton from "./Youtube";
+const user = {
+  name: "UserName",
+  age: 20,
+  email: "user@example.com",
+
+};
+
 function App() {
   return (
     <>
       <h1>Hello React</h1>
       <h1>hello</h1>
       <MyButton />
+      <h2>User Info</h2>
+      <p>Name : {user.name}</p>
+      <p>Age : {user.age}</p>
+      <p>Email : {user.email}</p>
+
     </>
-   
   );
 }
 
