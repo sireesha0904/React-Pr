9bb292e99087404d5abd65dbd80487246ef65f1f
sireesha0904 <modisireesha09@gmail.com>
2024-12-06 19:11:46 +0530

    updated

--- react-app/src/Youtube.jsx
+++ react-app/src/Youtube.jsx
@@ -3,5 +3,7 @@ function Youtube() {
         <h1>Hey This is Youtube</h1>
     )
 }
-
+const myelement = <h1>Hello {5 + 5}</h1>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(myelement);
 export default Youtube
