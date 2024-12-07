46f85df12641dd184276a209a97dd15a758aeb5c
sireesha0904 <modisireesha09@gmail.com>
2024-12-07 20:08:55 +0530

    PR

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,7 +1,16 @@
 
-import Ract, { Component } from 'react';
+import React, { Component } from 'react';
 class Greetings extends Component {
   render() {
     return <h1>Hello, {this.props.name}</h1>
   }
 }
+
+class App extends Component {
+  render() {
+    return <div>
+      <Greetings name = "React"/>
+    </div>
+  }
+}
+export default App;
