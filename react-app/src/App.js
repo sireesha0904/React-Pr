1b533f2cbb6bbbfc99381d0bb99c0561f3ea2727
sireesha0904 <modisireesha09@gmail.com>
2024-12-07 20:02:10 +0530

    updated

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,7 +1,4 @@
-function Greetings(props) {
-  return <h1>Hello, {props.name}</h1>
-}
-function App() {
-  return <Greetings name ="Property Name" />;
+function Greetings(props){
+  retur
 }
 export default App;
