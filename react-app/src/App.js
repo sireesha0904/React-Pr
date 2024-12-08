7305b15f2dbc77d0a489a4753e3db56759c0551f
sireesha0904 <modisireesha09@gmail.com>
2024-12-08 21:28:20 +0530

    App

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,16 +1,11 @@
+import NewsFeed from "./NewsFeed";
 
-import React, { Component } from 'react';
-class Greetings extends Component {
-  render() {
-    return <h1>Hello, {this.props.name}</h1>
-  }
-}
-
-class App extends Component {
-  render() {
-    return <div>
-      <Greetings name = "React"/>
+function App() {
+  return (
+    <div>
+      <NewsFeed/>
     </div>
-  }
+  )
 }
+
 export default App;
