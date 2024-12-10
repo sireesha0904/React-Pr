1cce698e103e7e02d23acd0edf60f935101252c0
sireesha0904 <modisireesha09@gmail.com>
2024-12-10 21:17:52 +0530

    pr

--- react-app/src/Practice.jsx
+++ react-app/src/Practice.jsx
@@ -1,7 +1,16 @@
 
 import React, { useState } from "react";
 function Greetings() {
-    
+    const [isLoggin, SetLogin] = useState(false);
+    return (
+        <div>
+            <h1>Hello, Welcome to this Page</h1>
+            {isLoggin ? <h1>Welcome User ! </h1> : <h1>Please Login</h1>}
+            <button onClick={() => SetLogin(!isLoggin)}>
+                Login Status
+            </button>
+        </div>
+    );
 }
 
 export default Greetings;
