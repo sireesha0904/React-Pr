2d9745308b1235d086f595cc7c5a8afdd7597ed2
sireesha0904 <modisireesha09@gmail.com>
2024-12-10 21:33:01 +0530

    Pr

--- react-app/src/DataFetcher.jsx
+++ react-app/src/DataFetcher.jsx
@@ -1,5 +1,17 @@
 import React, {useState, useEffect} from "react";
 function DataFetcher() {
-    
+    const [data, Setdata] = useState(null);
+    const [isLoading, SetLoading] = useEffect(true);
+
+    useEffect(
+        () => { // Arrow functions
+
+        }
+    );
+    return (
+        <div>
+            {isLoading ? <p>Data is Loading.....</p> : <p>{data}</p>}
+        </div>
+    );
 }
 export default DataFetcher;
