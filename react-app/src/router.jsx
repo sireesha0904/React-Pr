1adb82cf69dcb4ef7a4c8b5e4ea82e7becb47d7d
sireesha0904 <modisireesha09@gmail.com>
2024-12-20 20:44:50 +0530

    pr

--- react-app/src/router.jsx
+++ react-app/src/router.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as router, Routes, Route, Link, BrowserRouter, } from "react-router-dom";
+import { BrowserRouter as Routes, Route, Link, BrowserRouter, } from "react-router-dom";
 import Home from "./home";
 import About from "./about";
 
