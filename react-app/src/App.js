bed4eaf5d43f98f43f5dc5ed87453ebe22c58eb7
sireesha0904 <modisireesha09@gmail.com>
2024-12-20 20:52:26 +0530

    pr

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,8 +1,11 @@
+import React from "react";
 import Parent from "./Parent";
+import router from "./router";
 function App() {
   return (
     <div>
       <Parent />
+      <router/>
     </div>
   );
 }
