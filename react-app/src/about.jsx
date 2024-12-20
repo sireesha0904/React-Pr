a54e3db202a1a733041fe7f535cb8a2f94576111
sireesha0904 <modisireesha09@gmail.com>
2024-12-20 20:37:18 +0530

    Pr

---
+++ react-app/src/about.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function About() {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p>This is an about page.</p>
+        </div>
+    )
+}
+export default About;
