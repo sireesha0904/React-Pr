1a1f8759f31fb4b440e33114dbdad68b30ea708e
sireesha0904 <modisireesha09@gmail.com>
2024-12-20 21:23:41 +0530

    PR

--- react-app/src/routpr.jsx
+++ react-app/src/routpr.jsx
@@ -1,25 +1,20 @@
 import React from "react";
-import {
-  BrowserRouter as Routes,
-  Route,
-  Link,
-  BrowserRouter,
-} from "react-router-dom";
-
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./home";
 import About from "./about";
 
 function RouterPr() {
-    return (
-      <BrowserRouter>
-        <nav>
-          <Link to="/">home</Link> | <Link to="/about">About</Link>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
 export default RouterPr;
