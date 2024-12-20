c12c2d5fb7369b543077ec87ee715e063ad6ea4f
sireesha0904 <modisireesha09@gmail.com>
2024-12-20 20:44:18 +0530

    Pr

--- react-app/src/router.jsx
+++ react-app/src/router.jsx
@@ -1,2 +1,19 @@
 import React from "react";
-import { BrowserRouter as router, Routes, Route, } from "react-router-dom";
+import { BrowserRouter as router, Routes, Route, Link, BrowserRouter, } from "react-router-dom";
+import Home from "./home";
+import About from "./about";
+
+function Router() {
+    return (
+      <BrowserRouter>
+        <nav>
+          <Link to="/">home</Link> | <Link to="/about">About</Link>
+        </nav>
+        <Routes>
+          <Route exact path="/" Component={Home} />
+          <Route exact path="/about" Component={About} />
+        </Routes>
+      </BrowserRouter>
+    );
+}
+export default Router;
