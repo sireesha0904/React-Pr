05c59adbf4174f5890a9d7171d7e70efd803a24e
sireesha0904 <modisireesha09@gmail.com>
2024-12-23 21:42:12 +0530

    Pr

--- react-app/src/about.jsx
+++ react-app/src/about.jsx
@@ -2,7 +2,7 @@ import React from "react";
 function About() {
     return (
         <div>
-            <h1>About Page</h1>
+            <h1>About Section</h1>
             <p>This is an about page.</p>
         </div>
     )
