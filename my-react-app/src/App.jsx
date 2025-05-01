ae4142b8f48b9c0dc79ca4e49022cb98b4444e3b
sireesha0904 <modisireesha09@gmail.com>
2025-05-01 19:14:35 +0530

    updated

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,7 +1,11 @@
+import Header from './Header'
 
 
 function App() {
 
+  return (
+    <Header></Header>
+  );
 }
 
 export default App
---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+    return (
+        <header>
+            <h1>My React App</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li> 
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
+// This is a simple React component that serves as a header for the application.
