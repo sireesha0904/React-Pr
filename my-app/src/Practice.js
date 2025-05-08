9527dc6434a308e054700f1961fcba401c300bb1
sireesha0904 <modisireesha09@gmail.com>
2025-05-08 16:57:57 +0530

    practice

--- my-app/src/Practice.js
+++ my-app/src/Practice.js
@@ -3,10 +3,19 @@ import './practice.css';
 let name = "Sireesha";
 function Practice() {
   return (
-      <section>
-          <h1>Hello { name}, Welcome to React</h1>
+    <section className="section-container">
+      <h1 className="main-heading">Hello {name}, Welcome to React</h1>
+      <p className="para">
+        This is the first paragraph of the section. In Practice component.
+      </p>
+      <p className="para">
+        React allows you to create reusable components and manage state easily.
+      </p>
+      <p className="highlight">
+        You are doing great! Keep exploring React and building amazing apps.
+      </p>
     </section>
-  )
+  );
 }
 
 export default Practice
