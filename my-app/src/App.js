15f812e7b9d1cd67be8de3e7273b9a5fdb61eb93
sireesha0904 <modisireesha09@gmail.com>
2025-05-08 16:52:21 +0530

    practice

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,16 @@
 import Comp from './component';
 import Sample from './Example/Sample';
-import './sample.css';
+import Practice from './Practice';
+import './App.css';
+
 function App() {
   return (
     <div>
       <Comp />
       <Sample />
-      <h1 className="my-special-heading">This is my custom heading</h1>
+      <h1 className='heading'>Hello This is Sample Css Styles</h1>
+      <hr></hr>
+      <Practice />
     </div>
   );
 }
---
+++ my-app/src/Practice.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './practice.css';
+let name = "Sireesha";
+function Practice() {
+  return (
+      <section>
+          <h1>Hello { name}, Welcome to React</h1>
+    </section>
+  )
+}
+
+export default Practice
