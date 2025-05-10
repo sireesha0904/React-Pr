987c0125afd36638428ff27feaeca433a795ee0c
sireesha0904 <modisireesha09@gmail.com>
2025-05-10 17:46:59 +0530

    Events

--- my-app/src/Events.js
+++ my-app/src/Events.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Events = () => {
+    const [message, setMessage] = useState("Welcocme to Events");
+    
+    const handleEvents = () => {
+        setMessage("Welcome to Events Handling");
+    }
   return (
-    <div>Events</div>
+      <div>{message} <br></br>
+          <button onClick={handleEvents}>Click Me</button>
+          
+          
+    </div>
     
   )
 }
