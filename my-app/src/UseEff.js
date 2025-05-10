3b76a17f7d7107720eef85b302327fdd961f6c2d
sireesha0904 <modisireesha09@gmail.com>
2025-05-10 17:08:23 +0530

    useEffect

--- my-app/src/UseEff.js
+++ my-app/src/UseEff.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 const UseEff = () => {
+    const [city, setCity] = useState("Parlakhemundi");
+    
+    if()
   return (
     <div>
-      
+      <p>Hello, I am from {city}</p>
     </div>
   )
 }
