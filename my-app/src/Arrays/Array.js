829f2d38c810940972658dc4e927e5966a0d999d
sireesha0904 <modisireesha09@gmail.com>
2025-05-13 15:20:23 +0530

    Array

---
+++ my-app/src/Arrays/Array.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Array = () => {
+    let arr = ["Sireesha", 21, "CSE", {city:"Parlakhemundi", state:"Odisha"}, "B.tech"]
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Array
