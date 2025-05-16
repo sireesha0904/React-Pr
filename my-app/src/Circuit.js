2fea151ed8ec6146662f042f6cb6b5c043db3474
sireesha0904 <modisireesha09@gmail.com>
2025-05-16 11:46:19 +0530

    Short-Circuit Evaluation

--- my-app/src/Circuit.js
+++ my-app/src/Circuit.js
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import useState from 'react'
 const Circuit = () => {
+
+    const [login, setlogin] = useState(true);
+
+
   return (
     <div>
-      
+          {login
+           && <h1>Welcome to Circuit</h1>}
     </div>
   )
 }
