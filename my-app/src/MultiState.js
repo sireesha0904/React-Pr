1bdb44f0b4197a273cdeb107bd7ac01d42c413ce
sireesha0904 <modisireesha09@gmail.com>
2025-05-17 10:47:09 +0530

    Multi State

--- my-app/src/MultiState.js
+++ my-app/src/MultiState.js
@@ -1,5 +1,13 @@
 import React from 'react'
 
+
+// types
+
+const FETCH_INIT= 'FETCH_INIT'
+const FETCH_START = 'FETCH_START'
+const FETCH_ERROR = 'FETCH_ERROR'
+
+
 const MultiState = () => {
   return (
     <div>
